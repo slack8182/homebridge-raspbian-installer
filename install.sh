eaa27157f0b7a6155325b00e45e44a7758bc6780
slack8182 <johnjackfert@yahoo.com>
2017-01-05 17:12:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,29 +6,21 @@ sudo apt-get upgrade -y
 
 sudo apt-get install -y git make libavahi-compat-libdnssd-dev dialog
 
-if [ "x$(uname -m)" != "xarmv6l" ]; then
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-else
-    wget https://nodejs.org/dist/v6.9.0/node-v6.9.0-linux-armv6l.tar.xz
-    sudo tar -xf node-v6.9.0-linux-armv6l.tar.xz
-    cd node-v6.9.0-linux-armv6l/
-    sudo cp -R * /usr/
-    cd -
+wget https://nodejs.org/dist/v7.4.0/node-v7.4.0-linux-armv6l.tar.xz
+sudo tar -xf node-v7.4.0-linux-armv6l.tar.xz
+cd node-v7.4.0-linux-armv6l/
+sudo cp -R * /usr/local/
+cd
 fi
 sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp json
-cd /usr/lib/node_modules/homebridge/
+cd /usr/local/lib/node_modules/homebridge/
 sudo npm install --unsafe-perm bignum
 cd -
-cd /usr/lib/node_modules/hap-nodejs/node_modules/mdns
+cd /usr/local/lib/node_modules/hap-nodejs/node_modules/mdns
 sudo node-gyp BUILDTYPE=Release rebuild
 cd -
-sudo useradd --system --user-group homebridge
-sudo mkdir -p /var/homebridge
-sudo cp configs/config.json /var/homebridge/config.json
-sudo chown -R homebridge:homebridge /var/homebridge/
-sudo cp files/homebridge /etc/default/homebridge
-sudo cp files/homebridge.service /etc/systemd/system/homebridge.service
+cd /etc/default && sudo wget https://www.dropbox.com/s/efhj7bll9yq5tgl/homebridge
+cd /etc/systemd/system && sudo wget https://www.dropbox.com/s/k610k1ens6ozsvb/homebridge.service
 sudo systemctl daemon-reload
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
