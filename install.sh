c94f365363d41ac9d5517624029d6811b3372826
slack8182 <johnjackfert@yahoo.com>
2017-01-05 19:56:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,10 @@ sudo tar -xf node-v7.4.0-linux-armv6l.tar.xz
 cd node-v7.4.0-linux-armv6l/
 sudo cp -R * /usr/local/
 cd
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
 sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp json
 cd /usr/local/lib/node_modules/homebridge/
 sudo npm install --unsafe-perm bignum
